e5d78fb44367c27e81d8cbb564b7c486d86875e9
Aravind V V <arvndvv@gmail.com>
2021-01-23 14:13:04 +0530

    fix

--- src/app/users/routes.js
+++ src/app/users/routes.js
@@ -3,7 +3,12 @@ const { validateUser } = require("../middlewares/authmiddleware");
 const userController = require("./controller");
 router.post("/register", userController.regUser);
 router.post("/login", userController.authUser);
-router.get("/",validateUser, userController.bypassLogin);
+app.get('/', function(req, res) {
+
+    res.sendFile(path.join(__dirname + './frontEnd/dist/expense-app/index.html'));
+});
+
+router.use("/", validateUser, userController.bypassLogin);
 // router.get("/", cartController.getCart);
 // router.delete("/empty-cart", cartController.emptyCart);
 // router.delete('/subtract/:id', cartController.subtractItem);
